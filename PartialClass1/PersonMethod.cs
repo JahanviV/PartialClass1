43fdbccba9eee5d4bee72564bb5fc7c2e795607e
JahanviV <jahanvivarshney112@gmail.com>
2023-08-28 08:53:10 +0530

    Add project files.

---
+++ PartialClass1/PersonMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass1
+{
+    using System;
+
+    public partial class Person
+    {
+        public void PrintFullName()
+        {
+            Console.WriteLine($"Full Name: {FirstName} {LastName}");
+        }
+    }
+
+}
---
+++ PartialClass1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "John";
+            person.LastName = "Doe";
+
+            person.PrintFullName();
+        }
+    }
+
+}
